58add338fbc42d34f94627fd221f652c6af38f63
aimecitar <maimesaracho@gmail.com>
2021-08-17 18:32:54 -0300

    clase 1 de javascript

---
+++ Js/holaMundo.js
@@ -0,0 +1,33 @@
+//mostrar un mensaje por consola
+console.log("hola comision 17");
+
+//mostrar un mensaje en la pagina html
+document.write("<h1>Este es mi primer mensaje</h1>");
+
+//mostrar una ventana emergente
+//alert("esto es una alerta");
+
+// crear o declarar variables reservadas var let y const
+
+//var y let pueden almacenar un valor pero pueden cambiar a lo largo del programa
+//const tiene un único valor para siempre
+
+let palabraBuscada = "hola";
+
+document.write("<br>Variable palabra buscada: " + palabraBuscada);
+
+//modificar el valor de una variable
+
+palabraBuscada = "página de blog";
+
+document.write("<br>Variable palabra buscada: " + palabraBuscada);
+
+const comision = 17;
+
+document.write("<br> Consante comisión: " + comision);
+
+let luz = true;
+
+let suma = 20;
+console.log(--suma);
+console.log(-suma);
